54ac0ca0e3b44ec4056e61d175cb28290be91afb
BAAFI MARCUS <marcusowusu26@gmail.com>
2025-12-13 21:14:28 +0100

    Fix Runtime Error: Restore missing FaUser and FaPhone imports in Login.tsx

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -11,7 +11,7 @@ import {
     linkWithCredential,
     EmailAuthProvider
 } from '../../services/firebase';
-import { FaEnvelope, FaLock, FaGoogle } from 'react-icons/fa';
+import { FaEnvelope, FaLock, FaGoogle, FaUser, FaPhone } from 'react-icons/fa';
 import logo from '../../assets/logo.jpg';
 import { UserService } from '../../services/neon';
 
